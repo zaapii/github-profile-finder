d9350fd576840d902412f4800bff7ab8ac494290
Imanol <zaapii@gmail.com>
2023-01-27 20:59:53 -0300

    fetch users and added githubcontext

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useContext } from "react";
+import { CircleLoader } from "react-spinners";
+import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
+
+const UserResults = () => {
+
+  const { users, loading, fetchUsers } = useContext(GithubContext)
+
+  useEffect(() => {
+    fetchUsers()
+  }, []);
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    )
+  } else {
+    return <CircleLoader className="mx-auto" color="#36d7b7" size="150" />
+  }
+};
+
+export default UserResults;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
+import UserResults from "../components/users/UserResults"
+
 const Home = () => {
   return (
-    <h1 className="text-6xl">Welcome</h1>
+    <UserResults />
   )
 }
 
